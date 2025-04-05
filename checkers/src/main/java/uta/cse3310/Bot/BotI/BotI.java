2a1638a8781dd27de767bae9c655a55109e0b5d4
ktf831050 <99989742+ktf831050@users.noreply.github.com>
2025-04-04 23:16:59 -0500

    Update BotI.java
    kkkk

--- checkers/src/main/java/uta/cse3310/Bot/BotI/BotI.java
+++ checkers/src/main/java/uta/cse3310/Bot/BotI/BotI.java
@@ -1,12 +1,11 @@
 package uta.cse3310.Bot.BotI;
 
-import java.util.ArrayList;
-
 import uta.cse3310.GameManager.GameManager;
-
-// TODO: Temporary
+import java.util.ArrayList;
+//Todo: Temporary
 class Pieces {
 };
+
 class Move {
 };
 
@@ -14,20 +13,19 @@ public class BotI {
 
 	private GameManager gameManager;
 
+
 	public BotI(GameManager gameManager) {
-		// BotI needs a reference to GameManager, as this is how it will communicate
-		// with the game (e.g., receiving notice a user has made a move, retrieving
-		// the current state of the checkers board)
-		this.gameManager = gameManager;
+	// BotI needs a reference to GameManager, as this is how it will communicate
+        // with the game (e.g., receiving notice a user has made a move, retrieving
+        // the current state of the checkers board)
+	this.gameManager = gameManager;
 	}
-
-
 	// Checks if a potential move is legal
 	// TODO: Implement
+
 	private boolean validateMove(Pieces currentBoard, Move m) {
 		return false;
 	}
-
 	// Generates a complete list of (legal) moves the bot can make
 	// TODO: Implement
 	private ArrayList<Move> getAvailableMoves(Pieces currentBoard) {
@@ -35,8 +33,7 @@ public class BotI {
 		// Any moves added to availableMoves should be validated with validateMove()
 		return availableMoves;
 	}
-
-	// Takes the current 8x8 board state (X = human, O = bot, V = valid, ! = invalid),
+	// Takes the current 8x8 board state (X = human, 0 = bot, V = valid, ! = invalid),
 	// and returns a (legal/validated) move
 	// TODO: Implement
 	private Move generateMove(Pieces currentBoard) {
@@ -46,18 +43,17 @@ public class BotI {
 		Move tmp = new Move();
 		return tmp;
 	}
-
 	// This method gets called by the connected GameManager whenever a user has
-	// made their move. 
-	// TODO: Implement
+	// made their move.
+	//Todo: Implement
 	public void onUserMove(Pieces currentBoard, Move userMove) {
 		Move botMove = generateMove(currentBoard);
-		
 		// botMove will be null if no moves are available. The game should end here
 		if (botMove == null) {
-			// Game should end here
+	
+		// Game should end here
 		} else {
-			// Make move
+		// Make move
 		}
 	}
 }
